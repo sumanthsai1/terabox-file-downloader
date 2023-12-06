900b6e608c48162d8835ada850b87ec40f9031d9
sumanthsai1 <41504632+sumanthsai1@users.noreply.github.com>
2023-12-06 20:46:25 +0530

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -1,53 +1,3 @@
-import { NextResponse } from "next/server";
-import axios from "axios";
-
-import CryptoJS from "crypto-js";
-
-function getFormattedSize(sizeBytes) {
-  let size, unit;
-
-  if (sizeBytes >= 1024 * 1024) {
-    size = sizeBytes / (1024 * 1024);
-    unit = "MB";
-  } else if (sizeBytes >= 1024) {
-    size = sizeBytes / 1024;
-    unit = "KB";
-  } else {
-    size = sizeBytes;
-    unit = "bytes";
-  }
-
-  return `${size.toFixed(2)} ${unit}`;
-}
-
-function findBetween(str, start, end) {
-  const startIndex = str.indexOf(start) + start.length;
-  const endIndex = str.indexOf(end, startIndex);
-  return str.substring(startIndex, endIndex);
-}
-
-const headers = {
-  Accept:
-    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-  "Accept-Language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,hi;q=0.6",
-  Connection: "keep-alive",
-  Cookie:
-    "csrfToken=x0h2WkCSJZZ_ncegDtpABKzt; browserid=Bx3OwxDFKx7eOi8np2AQo2HhlYs5Ww9S8GDf6Bg0q8MTw7cl_3hv7LEcgzk=; lang=en; TSID=pdZVCjBvomsN0LnvT407VJiaJZlfHlVy; __bid_n=187fc5b9ec480cfe574207; ndus=Y-ZNVKxteHuixZLS-xPAQRmqh5zukWbTHVjen34w; __stripe_mid=895ddb1a-fe7d-43fa-a124-406268fe0d0c36e2ae; ndut_fmt=FF870BBFA15F9038B3A39F5DDDF1188864768A8E63DC6AEC54785FCD371BB182",
-  DNT: "1",
-  Host: "www.1024tera.com",
-  "Sec-Fetch-Dest": "document",
-  "Sec-Fetch-Mode": "navigate",
-  "Sec-Fetch-Site": "none",
-  "Sec-Fetch-User": "?1",
-  "Upgrade-Insecure-Requests": "1",
-  "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
-  "sec-ch-ua":
-    '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-  "sec-ch-ua-mobile": "?0",
-  "sec-ch-ua-platform": '"Windows"',
-};
-
 export async function GET(req, res) {
   const { searchParams: params } = new URL(req.url);
   if (!params.has("data") || !params.has("index")) {
@@ -59,6 +9,7 @@ export async function GET(req, res) {
   if (!encryptedData || isNaN(index)) {
     return NextResponse.json({ error: "Invalid data" }, { status: 400 });
   }
+
   const secretKey = "1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d";
   let url;
   try {
@@ -77,6 +28,7 @@ export async function GET(req, res) {
       { status: 400 }
     );
   }
+
   try {
     const req = await axios.get(url, { headers, withCredentials: true });
     const responseData = req.data;
@@ -85,6 +37,7 @@ export async function GET(req, res) {
     if (!jsToken || !logid) {
       return NextResponse.json({ error: "Invalid response" }, { status: 400 });
     }
+
     const { searchParams: requestUrl, href } = new URL(
       req.request.res.responseUrl
     );
@@ -93,7 +46,7 @@ export async function GET(req, res) {
     }
     const surl = requestUrl.get("surl");
 
-    const params = {
+    const listParams = {
       app_id: "250528",
       web: "1",
       channel: "dubox",
@@ -109,18 +62,17 @@ export async function GET(req, res) {
       root: "1",
     };
 
-      try {
     const req2 = await axios.get("https://www.1024tera.com/share/list", {
-      params,
+      params: listParams,
       headers,
       withCredentials: true,
     });
     const responseData2 = req2.data;
-    if (!"list" in responseData2) {
+    if (!("list" in responseData2)) {
       return NextResponse.json({ error: "Invalid response" }, { status: 400 });
     }
 
-    return NextResponse.json(responseData2?.list[0], { status: 200 });
+    return NextResponse.json(responseData2?.list[index], { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: "Unknown Error" }, { status: 400 });
   }

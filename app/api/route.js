d9728647ddeff2a133c8bce7c2396d1ba91361d1
sumanthsai1 <41504632+sumanthsai1@users.noreply.github.com>
2023-12-06 20:13:33 +0530

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -34,7 +34,7 @@ const headers = {
   Cookie:
     "csrfToken=x0h2WkCSJZZ_ncegDtpABKzt; browserid=Bx3OwxDFKx7eOi8np2AQo2HhlYs5Ww9S8GDf6Bg0q8MTw7cl_3hv7LEcgzk=; lang=en; TSID=pdZVCjBvomsN0LnvT407VJiaJZlfHlVy; __bid_n=187fc5b9ec480cfe574207; ndus=Y-ZNVKxteHuixZLS-xPAQRmqh5zukWbTHVjen34w; __stripe_mid=895ddb1a-fe7d-43fa-a124-406268fe0d0c36e2ae; ndut_fmt=FF870BBFA15F9038B3A39F5DDDF1188864768A8E63DC6AEC54785FCD371BB182",
   DNT: "1",
-  Host: "www.4funbox.com",
+  Host: "www.1024tera.com",
   "Sec-Fetch-Dest": "document",
   "Sec-Fetch-Mode": "navigate",
   "Sec-Fetch-Site": "none",
